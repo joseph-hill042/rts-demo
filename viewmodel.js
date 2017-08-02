7d28dc10c06a8543c08671cd07a3dd38d8beda16
Joseph Hill <joseph.hill1@outlook.com>
2017-08-01 21:17:41 -0400

    Finished string rotator

--- viewmodel.js
+++ viewmodel.js
@@ -1,4 +1,5 @@
 const myViewModel = {
+    // Observables and functions for above and below array splitter section
     selectedArray: ko.observable(''),
     inputForArray: ko.observable(''),
     numberAbove: ko.observable(''),
@@ -6,11 +7,10 @@ const myViewModel = {
     splitSelectedArray: function () {
         const input = myViewModel.inputForArray();
         let arr = myViewModel.selectedArray();
-        //console.log(typeof arr);
+        
         let nums = arr.replace(/[\[\]]/g, "").split(',').map(function(val) {
             return parseInt(val);
         });
-        console.log(nums);
         
         let greater = 0;
         let lower = 0;
@@ -25,12 +25,27 @@ const myViewModel = {
         });
         myViewModel.numberAbove(greater);
         myViewModel.numberBelow(lower);
-        console.log('above:' + greater + ', below:' + lower);
     },
     resetValues: function() {
         myViewModel.selectedArray('');
         myViewModel.inputForArray('');
         myViewModel.numberAbove('');
         myViewModel.numberBelow('');
+        myViewModel.stringToRotate('');
+        myViewModel.rotateNum('');
+        myViewModel.rotatedString('');
+    },
+    // Observables and functions for string rotator
+    stringToRotate: ko.observable(''),
+    rotateNum: ko.observable(''),
+    rotatedString: ko.observable(''),
+    rotator: function () {
+        let str = myViewModel.stringToRotate();
+
+        let sliced = str.slice(-parseInt(myViewModel.rotateNum()));
+
+        let leftOver = str.substr(0, (str.length - parseInt(myViewModel.rotateNum())));
+
+        myViewModel.rotatedString(sliced + leftOver);
     }
 }
